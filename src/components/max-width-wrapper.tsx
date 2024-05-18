5c7d1ea710ef273c7f65055cd551c58fce75bf27
Saufi <saufi1115@gmail.com>
2024-05-19 01:07:00 +0800

    refactor: 2xl

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils'
 import { type ReactNode } from 'react'
 
 function MaxWidthWrapper({ className, children }: { className?: string; children: ReactNode }) {
-  return <div className={cn('mx-auto w-full max-w-screen-xl px-3 md:px-20', className)}>{children}</div>
+  return <div className={cn('mx-auto w-full max-w-screen-2xl px-3 lg:px-20', className)}>{children}</div>
 }
 
 export default MaxWidthWrapper
