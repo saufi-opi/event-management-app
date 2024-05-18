d4f03e728e798241bb86719f12861e2ffa5fe50a
Saufi <saufi1115@gmail.com>
2024-05-19 01:08:36 +0800

    feat: delete button

---
+++ src/components/delete-button.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react'
+import { Button } from './ui/button'
+
+interface Props {
+  id: string | number
+  action: unknown
+}
+
+function DeleteButton(props: Props) {
+  const handleClick = async () => {
+    if (typeof props.action === 'function') {
+      await props.action(props.id)
+    }
+  }
+  return (
+    <Button size="sm" variant="ghost" onClick={() => handleClick()}>
+      <p className="text-destructive">Delete</p>
+    </Button>
+  )
+}
+
+export default DeleteButton
