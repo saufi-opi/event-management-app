9e13beeda75048e684b897de01a50c91bf95f418
Saufi <saufi1115@gmail.com>
2024-05-19 02:55:53 +0800

    style: icon animate on hover

--- src/components/navigation/nav-link.tsx
+++ src/components/navigation/nav-link.tsx
@@ -10,9 +10,9 @@ export default function NavLink({ href, title, icon }: Menu) {
   const isActive = pathName.startsWith(href)
 
   return (
-    <div className={`text-gray-300 duration-300 hover:text-gray-100 ${pathName !== href && 'text-gray-400'}`}>
-      <Link className={`flex gap-3 py-3 pl-10 ${isActive && 'bg-primary text-secondary'}`} href={href}>
-        <span className="h-6 w-6">{icon}</span>
+    <div className={`text-gray-300 duration-300 hover:text-gray-100 hover:bg-primary/20 ${pathName !== href && 'text-gray-400'}`}>
+      <Link className={`group flex gap-3 py-3 pl-10 ${isActive && 'bg-primary text-secondary'}`} href={href}>
+        <span className="h-6 w-6 group-hover:animate-bounce">{icon}</span>
         <span>{title}</span>
       </Link>
     </div>
