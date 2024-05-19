3a4bfd4e35e9b200d92056474db22023c75c9cdd
Saufi <saufi1115@gmail.com>
2024-05-19 13:07:21 +0800

    refatcor: required message and min message

--- types/zod/event.zod.ts
+++ types/zod/event.zod.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod'
 
 export const EventZodSchema = z.object({
-  name: z.string().min(1, { message: 'Event name is required' }).max(50, { message: 'Event name cannot be more than 50 characters' }),
-  location: z.string().min(1, { message: 'Event location is required' }),
-  description: z.string().max(200, { message: 'Event description cannot be more than 200 characters' }).optional(),
-  date: z.date({ required_error: 'event date is required' }).default(new Date())
+  name: z
+    .string({ required_error: 'Event name is required' })
+    .min(1, { message: 'Event name is required' })
+    .max(50, { message: 'Event name cannot be more than 50 characters' }),
+  location: z.string({ required_error: 'Event location is required' }).min(1, { message: 'Event location is required' }),
+  description: z
+    .string({ required_error: 'Event description is required' })
+    .min(1, { message: 'Event description is required' })
+    .max(200, { message: 'Event description cannot be more than 200 characters' }),
+  date: z.date({ required_error: 'Event date is required' })
 })
