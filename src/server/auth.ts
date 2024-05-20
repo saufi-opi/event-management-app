7f9b8e24193c291c740c7bce71e3ca6c9913892d
Saufi <saufi1115@gmail.com>
2024-05-20 17:14:58 +0800

    feat: GoogleProvider from next-auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth'
 import { type Adapter } from 'next-auth/adapters'
-import DiscordProvider from 'next-auth/providers/discord'
+import Googleprovider from 'next-auth/providers/google'
 
 import { env } from '@/env'
 import { db } from '@/server/db'
@@ -44,19 +44,14 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    DiscordProvider({
+    Googleprovider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET
     })
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    // FacebookProvider({
+    //   clientId: env.GOOGLE_CLIENT_ID,
+    //   clientSecret: env.GOOGLE_CLIENT_SECRET
+    // })
   ]
 }
 
