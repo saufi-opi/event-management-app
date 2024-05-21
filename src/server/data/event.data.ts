bce8364bd271f104ddb03ae35eed8f06758416c0
Saufi <saufi1115@gmail.com>
2024-05-21 13:25:46 +0800

    feat: filter with casl permission

--- src/server/data/event.data.ts
+++ src/server/data/event.data.ts
@@ -4,8 +4,14 @@ import { type CommonQuery } from 'types/query'
 import { db } from '../db'
 import { type Event } from '@prisma/client'
 import { type ReturnOne, type ReturnMany } from 'types/response'
+import { buildAbilityFor } from '../permission/permission'
+import { getServerAuthSession } from '../auth'
+import { accessibleBy } from '@casl/prisma'
 
 export const getAllEvents = async (query: CommonQuery): Promise<ReturnMany<Event>> => {
+  const session = await getServerAuthSession()
+  const ability = buildAbilityFor(session?.user.id)
+
   const page = query.page ?? 1
   const pageSize = query.pageSize
   const search = query.search ?? ''
@@ -14,18 +20,28 @@ export const getAllEvents = async (query: CommonQuery): Promise<ReturnMany<Event
   const [count, items] = await Promise.all([
     db.event.count({
       where: {
-        name: {
-          contains: search
-        }
+        AND: [
+          accessibleBy(ability).Event,
+          {
+            name: {
+              contains: search
+            }
+          }
+        ]
       }
     }),
     db.event.findMany({
       take: pageSize,
       skip,
       where: {
-        name: {
-          contains: search
-        }
+        AND: [
+          accessibleBy(ability).Event,
+          {
+            name: {
+              contains: search
+            }
+          }
+        ]
       }
     })
   ])
@@ -38,7 +54,14 @@ export const getAllEvents = async (query: CommonQuery): Promise<ReturnMany<Event
 }
 
 export const getEventById = async (id: string): Promise<ReturnOne<Event>> => {
-  const item = await db.event.findFirst({ where: { id } })
+  const session = await getServerAuthSession()
+  const ability = buildAbilityFor(session?.user.id)
+
+  const item = await db.event.findFirst({
+    where: {
+      AND: [accessibleBy(ability).Event, { id }]
+    }
+  })
 
   return {
     success: true,
--- src/server/data/participant.data.ts
+++ src/server/data/participant.data.ts
@@ -4,12 +4,18 @@ import { type CommonQuery } from 'types/query'
 import { db } from '../db'
 import { type Event, type Participant } from '@prisma/client'
 import { type ReturnOne, type ReturnMany } from 'types/response'
+import { accessibleBy } from '@casl/prisma'
+import { getServerAuthSession } from '../auth'
+import { buildAbilityFor } from '../permission/permission'
 
 export interface ParticipantWithEvent extends Participant {
   event: Event
 }
 
 export const getAllParticipants = async (query: CommonQuery): Promise<ReturnMany<ParticipantWithEvent>> => {
+  const session = await getServerAuthSession()
+  const ability = buildAbilityFor(session?.user.id)
+
   const page = query.page ?? 1
   const pageSize = query.pageSize
   const search = query.search ?? ''
@@ -18,9 +24,14 @@ export const getAllParticipants = async (query: CommonQuery): Promise<ReturnMany
   const [count, items] = await Promise.all([
     db.participant.count({
       where: {
-        name: {
-          contains: search
-        }
+        AND: [
+          accessibleBy(ability).Participant,
+          {
+            name: {
+              contains: search
+            }
+          }
+        ]
       }
     }),
     db.participant.findMany({
@@ -28,6 +39,7 @@ export const getAllParticipants = async (query: CommonQuery): Promise<ReturnMany
       skip,
       where: {
         AND: [
+          accessibleBy(ability).Participant,
           {
             name: {
               contains: search
@@ -51,7 +63,10 @@ export const getAllParticipants = async (query: CommonQuery): Promise<ReturnMany
 }
 
 export const getParticipantById = async (id: string): Promise<ReturnOne<Participant>> => {
-  const item = await db.participant.findFirst({ where: { id } })
+  const session = await getServerAuthSession()
+  const ability = buildAbilityFor(session?.user.id)
+
+  const item = await db.participant.findFirst({ where: { AND: [accessibleBy(ability).Participant, { id }] } })
 
   return {
     success: true,
