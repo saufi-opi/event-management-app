2bf313f953cb9ace35e971413bf6e25070c31b82
Saufi <saufi1115@gmail.com>
2024-05-21 13:23:47 +0800

    feat: event createdBy

--- src/server/actions/event.action.ts
+++ src/server/actions/event.action.ts
@@ -4,8 +4,13 @@ import { revalidatePath } from 'next/cache'
 import { db } from '../db'
 import { type Event } from '@prisma/client'
 import { type ReturnOne } from 'types/response'
+import { getServerAuthSession } from '../auth'
 
 export const createEvent = async (event: Partial<Event>): Promise<ReturnOne<Event>> => {
+  const session = await getServerAuthSession()
+  const createdBy = session?.user.id
+  event.createdBy = createdBy
+
   const item = await db.event.create({ data: event as Event })
 
   revalidatePath('/dashboard/event')
