f3d4b03e2e19a3731c40f31885e033eae2893b9b
Saufi <saufi1115@gmail.com>
2024-05-21 09:42:23 +0800

    refactor: auth midlleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,24 @@
-import { withAuth } from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export default withAuth({
-  pages: {
-    signIn: '/login',
-    error: '/error',
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname
+
+  if (path === '/') {
+    return NextResponse.next()
   }
-})
 
-export const config = { matcher: ['/dashboard/:path*'] }
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET
+  })
+
+  const isProtected = path.startsWith('/dashboard')
+
+  if (!session && isProtected) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  } else if (session && path === '/login') {
+    return NextResponse.redirect(new URL('/dashboard/event', req.url))
+  }
+  return NextResponse.next()
+}
