f8e05426a984e945f2fc22faa451faed22958492
Saufi <saufi1115@gmail.com>
2024-05-23 12:25:18 +0800

    feat: get error message function

---
+++ src/server/error.ts
@@ -0,0 +1,15 @@
+export function getErrorMessage(error: unknown) {
+  let message: string
+
+  if (error instanceof Error) {
+    message = error.message
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message)
+  } else if (typeof error === "string") {
+    message = error
+  } else {
+    message = "default-error-message"
+  }
+
+  return message
+}
