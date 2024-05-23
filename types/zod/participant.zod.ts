233ca48827bb97091c2863b4bef44b75579a46c3
Saufi <saufi1115@gmail.com>
2024-05-23 12:06:59 +0800

    feat: transform name to uppercase, email tolower case

--- types/zod/participant.zod.ts
+++ types/zod/participant.zod.ts
@@ -4,11 +4,13 @@ export const ParticipantZodSchema = z.object({
   name: z
     .string({ required_error: 'Participant name is required' })
     .min(1, { message: 'Participant name is required' })
-    .max(50, { message: 'Participant name cannot be more than 50 characters' }),
+    .max(50, { message: 'Participant name cannot be more than 50 characters' })
+    .transform((name) => name.toUpperCase()),
   phone: z.string({ required_error: 'Participant phone is required' }).min(1, { message: 'Participant phone is required' }),
   email: z
     .string({ required_error: 'Participant email is required' })
     .min(1, { message: 'Participant email is required' })
-    .email({ message: 'Invalid email' }),
+    .email({ message: 'Invalid email' })
+    .transform((email) => email.toLowerCase()),
   eventId: z.string({ required_error: 'Event is required' })
 })
