5d0458b9d93956caddd8ccede3ea4fd6875f9f7c
Saufi <saufi1115@gmail.com>
2024-05-23 11:53:24 +0800

    feat: using profile card

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AppMenu from './app-menu'
 import { cn } from '@/lib/utils'
 import LogoutButton from '../logout-button'
+import Profilecard from '../profile-card'
 
 interface Props {
   className?: string
@@ -10,10 +11,13 @@ interface Props {
 function Sidebar({ className }: Props) {
   return (
     <div className={cn('min-h-screen min-w-[300px] border-r-[1px] border-gray-900 bg-gray-900 text-white', className)}>
-      <div className="border-b border-gray-400 p-5">
+      <div className="border-b border-gray-400 py-5">
+        <Profilecard />
+      </div>
+      <div className="border-b border-gray-400 py-5">
         <h1 className="text-center text-xl font-bold">Event Management App</h1>
       </div>
-      <div className="flex flex-col gap-3 py-10">
+      <div className="flex flex-col gap-3 py-5">
         <AppMenu />
       </div>
       <div className="mx-auto my-5 w-3/4">
